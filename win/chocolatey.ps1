b2fcdd86948599f55a354e670f94e1b6f78d6448
anels <1177049+anels@users.noreply.github.com>
2021-11-25 10:29:13 -0800

    Add 'contextmenumanager'

--- win/chocolatey.ps1
+++ win/chocolatey.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 
-if ($null -ne (Get-Command choco).Name) {
+if ($null -eq (Get-Command choco).Name) {
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
@@ -8,6 +8,7 @@ choco upgrade -yr --no-progress `
   eartrumpet `
   7zip `
   cmder `
+  contextmenumanager `
   dismplusplus `
   ditto `
   duf `
